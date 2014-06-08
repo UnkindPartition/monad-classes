f553f11ebf15786a8922a313e52fd140b5cb7886
Roman Cheplyaka <roma@ro-che.info>
2014-06-08 10:47:14 +0200

    Initial code

---
+++ Control/Monad/Better/Core.hs
@@ -0,0 +1,7 @@
+module Control.Monad.Better.Core where
+
+data Nat = Zero | Suc Nat
+
+type family Find (t :: (* -> *) -> (* -> *)) (m :: * -> *) :: Nat where
+  Find t (t m) = Zero
+  Find t (p m) = Suc (Find t m)
---
+++ Control/Monad/Better/State.hs
@@ -0,0 +1,29 @@
+module Control.Monad.Better.State where
+import Control.Monad.Trans.State hiding (get, put)
+import qualified Control.Monad.Trans.State as Trans
+import Control.Monad.Trans.Class
+import Control.Applicative
+import Data.Tagged
+import Control.Monad.Better.Core
+
+class Monad m => MonadStateN (n :: Nat) s m where
+  getN :: Tagged n (m s)
+  putN :: Tagged n (s -> m ())
+
+instance Monad m => MonadStateN Zero s (StateT s m) where
+  getN = Tagged Trans.get
+  putN = Tagged Trans.put
+
+instance (MonadStateN n s m, Monad m)
+  => MonadStateN (Suc n) s (StateT s' m)
+  where
+    getN = Tagged . lift . untag $ (getN :: Tagged n (m s))
+    putN = Tagged . (lift .) . untag $ (putN :: Tagged n (s -> m ()))
+
+type MonadState s m = MonadStateN (Find (StateT s) m) s m
+
+get :: forall s m. (MonadState s m) => m s
+get = untag (getN :: Tagged (Find (StateT s) m) (m s))
+
+put :: forall s m . (MonadState s m) => s -> m ()
+put = untag (putN :: Tagged (Find (StateT s) m) (s -> m ()))
