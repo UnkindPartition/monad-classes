68f67acb56cbffcb14e3d9d02c5aa898f5117711
Roman Cheplyaka <roma@ro-che.info>
2014-06-17 13:57:56 +0300

    Expose exceptions

--- Control/Monad/Classes.hs
+++ Control/Monad/Classes.hs
@@ -15,18 +15,23 @@ module Control.Monad.Classes
     -- * Writer
   , MonadWriter
   , tell
+    -- * Exceptions
+  , MonadExcept
+  , throw
     -- * Core classes and types
     -- ** Effects
   , EffState
   , EffReader
   , EffLocal
   , EffWriter
+  , EffExcept
     -- ** N-classes
   , Nat(..)
   , MonadStateN(..)
   , MonadReaderN(..)
   , MonadLocalN(..)
   , MonadWriterN(..)
+  , MonadExceptN(..)
     -- ** Type families
     -- | You should rarely need these. They are exported mostly for
     -- documentation and pedagogical purposes.
@@ -40,3 +45,4 @@ import Control.Monad.Classes.Core
 import Control.Monad.Classes.State
 import Control.Monad.Classes.Reader
 import Control.Monad.Classes.Writer
+import Control.Monad.Classes.Except
