58a3ad5dad81b6384845635431ea609fad343c87
Roman Cheplyaka <roma@ro-che.info>
2014-06-17 16:23:11 +0300

    Fix type of throw

--- Control/Monad/Classes/Except.hs
+++ Control/Monad/Classes/Except.hs
@@ -41,5 +41,5 @@ instance (MonadTrans t, Monad (t m), MonadExceptN n e m, Monad m)
 type MonadExcept e m = MonadExceptN (Find (EffExcept e) m) e m
 
 -- | Throw an exception
-throw :: forall e m . MonadExcept e m => e -> m ()
+throw :: forall a e m . MonadExcept e m => e -> m a
 throw = throwN (proxy# :: Proxy# (Find (EffExcept e) m))
