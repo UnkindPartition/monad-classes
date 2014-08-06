bbdcb963956fb674e52b36bbdd05e4a3c43547b1
Roman Cheplyaka <roma@ro-che.info>
2014-08-06 19:05:18 +0300

    Export MonadTransControl associated type constructors
    This will soon become redundant, as they are moving to type synonyms

--- Control/Monad/Classes/Run.hs
+++ Control/Monad/Classes/Run.hs
@@ -30,6 +30,9 @@ module Control.Monad.Classes.Run
     -- * Zoom
   , runZoom
   , ZoomT(..)
+    -- 'MonadBaseControl' state
+  , StT(..)
+  , StM(..)
   ) where
 
 import Data.Functor.Identity
@@ -38,6 +41,7 @@ import Control.Monad.Classes.State
 import Control.Monad.Classes.Writer
 import Control.Monad.Classes.Reader
 import Control.Monad.Classes.Except
+import Control.Monad.Trans.Control (MonadTransControl(StT),MonadBaseControl(StM))
 
 run :: Identity a -> a
 run = runIdentity
